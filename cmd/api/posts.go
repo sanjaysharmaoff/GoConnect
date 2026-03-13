2b6f710404b00316548c34f7e4dd4f66ba31ba46
sanjaysharmaoff <sanjaysharmaoff@gmail.com>
2026-03-13 15:58:32 +0530

    added json structure

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -49,7 +49,7 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 		app.internalServerError(w, r, err)
 		return
 	}
-	if err := writeJSON(w, http.StatusCreated, post); err != nil {
+	if err := app.jsonResponse(w, http.StatusCreated, post); err != nil {
 		app.internalServerError(w, r, err)
 		return
 	}
@@ -65,7 +65,7 @@ func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	post.Comments = commments
 
-	if err := writeJSON(w, http.StatusOK, post); err != nil {
+	if err := app.jsonResponse(w, http.StatusOK, post); err != nil {
 		app.internalServerError(w, r, err)
 		return
 	}
@@ -114,7 +114,7 @@ func (app *application) updatePostHandler(w http.ResponseWriter, r *http.Request
 	if err := app.updatePost(ctx, post); err != nil {
 		app.internalServerError(w, r, err)
 	}
-	if err := writeJSON(w, http.StatusOK, post); err != nil {
+	if err := app.jsonResponse(w, http.StatusOK, post); err != nil {
 		app.internalServerError(w, r, err)
 	}
 }
